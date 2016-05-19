b49c5fe588b62cc422a74c33555e99d6cfd5208c
Simon Gawlik <sgawlik@gmail.com>
2016-05-19 08:31:32 -0400

    added way to use slugs instead of ids

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Post
 
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "created_date", "published_date"]
+    list_filter = ["created_date", "published_date"]
+    search_fields = ["title", "text"]
+    prepopulated_fields = {"slug": ("title",)}
+    class Meta:
+        model = Post
 
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,17 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+
 from .forms import PostForm
 from .models import Post
 
 # Create your views here.
 def post_list(request):
-	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
 	return render(request, 'blog/post_list.html', {'posts': posts})
 
-def post_detail(request, pk):
-	post = get_object_or_404(Post, pk=pk)
+def post_detail(request, slug):
+	post = get_object_or_404(Post, slug=slug)
 	return render(request, 'blog/post_detail.html', {'post': post})
 
 @login_required
@@ -21,14 +22,15 @@ def post_new(request):
 			post = form.save(commit=False)
 			post.author = request.user
 			post.save()
-			return redirect('post_detail', pk=post.pk)
+			return redirect('post_detail', slug=post.slug)
 	else:
 		form = PostForm()
-		return render(request, 'blog/post_edit.html', {'form': form})
+	return render(request, 'blog/post_edit.html', {'form': form})
+
 
 @login_required
-def post_edit(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+def post_edit(request, slug):
+    post = get_object_or_404(Post, slug=slug)
     if request.method == "POST":
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
@@ -36,24 +38,24 @@ def post_edit(request, pk):
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
-            return redirect('post_detail', pk=post.pk)
+            return redirect('post_detail', slug=post.slug)
     else:
-        form = PostForm(instance=post)
+    	form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
 
 @login_required
 def post_draft_list(request):
-	posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+	posts = Post.objects.filter(published_date__isnull=True).order_by('-created_date')
 	return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
 @login_required
-def post_publish(request, pk):
-	post = get_object_or_404(Post, pk=pk)
+def post_publish(request, slug):
+	post = get_object_or_404(Post, slug=slug)
 	post.publish()
-	return redirect('blog.views.post_detail', pk=pk)
+	return redirect('blog.views.post_detail', slug=slug)
 
 @login_required
-def post_remove(request, pk):
-	post = get_object_or_404(Post, pk=pk)
+def post_remove(request, slug):
+	post = get_object_or_404(Post, slug=slug)
 	post.delete()
 	return redirect('blog.views.post_list')
